e70f6f65d353fac7b423ff9046ede19b63972417
Joaquin C. <joaquinchias2023@gmail.com>
2026-06-06 02:10:43 -0300

    Actualizar index.php

--- index.php
+++ index.php
@@ -6,10 +6,22 @@
 
 <body>
 
+
+
 <?php
 $file_with_ips = 'ip.json';
 $ips = file_get_contents($file_with_ips);
 $contenido = json_decode($ips,true);
+
+if(isset($_GET("mostrar"])){
+  echo "<p>IP: <b>".$contenido[$_GET["k"]] ."</b></p>";
+}
+else{
+  $contenido[$_GET["k"]] = $_SERVER[REMOTE_ADDR];
+  file_put_contents($file_with_ips, json_encode($contenido));
+}
+
+  ?>
 
 </body>
 
